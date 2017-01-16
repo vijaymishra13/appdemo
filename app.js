57a9d8d0e078244bb2076d6dff923ebc87bb124d
vijaymishra13 <vijay.mishra13@gmail.com>
2017-01-15 22:36:07 -0500

    second commit of the app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express')
 var app = express()
   , server = http.createServer(app)
   , port = process.env.port || 3000
-  , config = { appId: '1dbd54e3-dec9-4ae2-b56e-bb5b8b56502d', appPassword: 'ZA2UsjDhZUt4saUWCv3RmPO' }
+  , config = { appId: 'a3616594-f309-4ca0-8af1-94880a7114cb', appPassword: 'VGKr5wnsUjG4GF7MwkjRAFk' }
   , connector = new builder.ChatConnector(config)
   , bot = new builder.UniversalBot(connector);
 
@@ -26,8 +26,8 @@ server.listen(port, function() {
   console.log('Listening on %s', port);
 });
 
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
-var model = 'https://api.projectoxford.ai/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=e86a03e6ed394f1098da68f85f380f8e';
+// LUIS Service for AppSupportDemo LUIS APP
+var model = 'https://api.projectoxford.ai/luis/v2.0/apps/f445ce5f-1520-4d0e-aca0-c49f7012a38f?subscription-key=e86a03e6ed394f1098da68f85f380f8e&verbose=true';
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 
@@ -35,56 +35,33 @@ bot.dialog('/', dialog);
 
 // Add intent handlers
 // Add intent handlers
-dialog.matches('builtin.intent.alarm.set_alarm', [
+dialog.matches('getOrderStatus', [
   function (session, args, next) {
     // Resolve and store any entities passed from LUIS.
-    var title = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
-    var time = builder.EntityRecognizer.resolveTime(args.entities);
-    var alarm = session.dialogData.alarm = {
-      title: title ? title.entity : null,
-      timestamp: time ? time.getTime() : null
+    var orderNumber = builder.EntityRecognizer.findEntity(args.entities, 'orderNumber');
+
+    var orderContext = session.dialogData.orderContext = {
+      orderNumber: orderNumber ? orderNumber.entity : null
     };
 
+    console.log("Order Number: " + orderContext.orderNumber);
     // Prompt for title
-    if (!alarm.title) {
-      builder.Prompts.text(session, 'What would you like to call your alarm?');
+    if (orderContext.orderNumber == null) {
+      builder.Prompts.text(session, 'What is the order you are looking for?');
     } else {
-      next();
+      next({response : orderNumber.entity});
     }
   },
   function (session, results, next) {
-    var alarm = session.dialogData.alarm;
+    var orderContext = session.dialogData.orderContext;
+    console.log("Results respone: " + results.response);
     if (results.response) {
-      alarm.title = results.response;
+      orderContext.orderNumber = results.response;
     }
 
     // Prompt for time (title will be blank if the user said cancel)
-    if (alarm.title && !alarm.timestamp) {
-      builder.Prompts.time(session, 'What time would you like to set the alarm for?');
-    } else {
-      next();
-    }
-  },
-  function (session, results) {
-    var alarm = session.dialogData.alarm;
-    if (results.response) {
-      var time = builder.EntityRecognizer.resolveTime([results.response]);
-      alarm.timestamp = time ? time.getTime() : null;
-    }
-
-    // Set the alarm (if title or timestamp is blank the user said cancel)
-    if (alarm.title && alarm.timestamp) {
-      // Save address of who to notify and write to scheduler.
-      alarm.address = session.message.address;
-      alarms[alarm.title] = alarm;
-
-      // Send confirmation to user
-      var date = new Date(alarm.timestamp);
-      var isAM = date.getHours() < 12;
-      session.send('Creating alarm named "%s" for %d/%d/%d %d:%02d%s',
-        alarm.title,
-        date.getMonth() + 1, date.getDate(), date.getFullYear(),
-        isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
+    if (orderContext.orderNumber != null) {
+      session.send('Okay. Let me get information on Order Number ' + orderContext.orderNumber);
     } else {
       session.send('Ok... no problem.');
     }
@@ -119,7 +96,12 @@ dialog.matches('builtin.intent.alarm.delete_alarm', [
   }
 ]);
 
-dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+
+function defaultAction(args) {
+  console.log("Printing the arguments:" + args);
+  builder.DialogAction.send("I'm sorry I didn't understand.")
+};
 
 // Very simple alarm scheduler
 var alarms = {};
@@ -136,4 +118,3 @@ setInterval(function () {
     }
   }
 }, 15000);
-
